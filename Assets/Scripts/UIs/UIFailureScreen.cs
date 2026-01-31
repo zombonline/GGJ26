0157a5e4f3948711501d968f6e59f5d0c32b147b
Kelvin <hoiman.k.yuen@gmail.com>
2026-01-31 19:03:34 +0000

    Added max combo to failure screen

--- Assets/Scripts/UIs/UIFailureScreen.cs
+++ Assets/Scripts/UIs/UIFailureScreen.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,9 +7,11 @@ public class UIFailureScreen : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private GameManager gameManager;
+    [SerializeField] private Player player;
     
     [Header("Components")]
     [SerializeField] private Animator animator;
+    [SerializeField] private TextMeshProUGUI comboText;
     [SerializeField] private Button retryButton;
     
     private static readonly int Showing = Animator.StringToHash("Showing");
@@ -33,6 +37,20 @@ public class UIFailureScreen : MonoBehaviour
         if (show)
         {
             retryButton.Select();
+            StartCoroutine(ShowComboAnimation());
         }
+    }
+
+    private IEnumerator ShowComboAnimation()
+    {
+        yield return new WaitForSecondsRealtime(0.25f);
+        float time = Time.unscaledTime;
+        float duration = 1f;
+        while (Time.unscaledTime - time < duration)
+        {
+            comboText.text = $"{(int)Mathf.Lerp(0, player.MaxCombo, (Time.unscaledTime - time) / duration)}";
+            yield return null;
+        }
+        comboText.text = $"{player.MaxCombo}";
     }
 }

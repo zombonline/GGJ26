9706ff814ac7ceca9021b6a8561b364c1eeab6b4
Kelvin <hoiman.k.yuen@gmail.com>
2026-02-01 13:12:34 +0000

    Modified countdown text

--- Assets/Scripts/UIs/UICountdownDisplay.cs
+++ Assets/Scripts/UIs/UICountdownDisplay.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class UICountdownDisplay : MonoBehaviour
 {
@@ -25,6 +23,7 @@ public class UICountdownDisplay : MonoBehaviour
     [SerializeField] private Transform textTransform;
     
     [Header("Settings")]
+    [SerializeField] private float delayBeforeCountdown;
     [SerializeField] private List<MessageInfo> messageInfos;
     
     private Coroutine _textFadeCoroutine;
@@ -33,23 +32,24 @@ public class UICountdownDisplay : MonoBehaviour
     {
         canvasGroup.alpha = 0f;
         textTransform.localScale = Vector3.zero;
-        StartCountdown();
     }
     
-    private void StartCountdown()
+    public void StartCountdown()
     {
         StartCoroutine(CountdownSequence());
     }
     
     private IEnumerator CountdownSequence()
     {
+        yield return new WaitForSeconds(delayBeforeCountdown);
+
         float secondPerBeat = 60f / songPlayer.currentChart.bpm;
         for (int i = 0; i < messageInfos.Count - 1; i++)
         {
-            ShowText(messageInfos[i], secondPerBeat * 1.5f);
+            ShowText(messageInfos[i], secondPerBeat * 2f);
             yield return new WaitForSeconds(secondPerBeat);
         }
-        ShowText(messageInfos[^1], secondPerBeat * 1.5f);
+        ShowText(messageInfos[^1], 1.5f);
         gameManager.CompleteCountdown();
         yield return null;
     }
@@ -61,25 +61,28 @@ public class UICountdownDisplay : MonoBehaviour
             StopCoroutine(_textFadeCoroutine);
             _textFadeCoroutine = null;
         }
-        StartCoroutine(ShowTextSequence(messageInfo, fullDuration));
+        _textFadeCoroutine = StartCoroutine(ShowTextSequence(messageInfo, fullDuration));
     }
 
     private IEnumerator ShowTextSequence(MessageInfo messageInfo, float fullDuration)
     {
         rimText.text = messageInfo.message;
-        rimText.color = messageInfo.color;
         countdownText.text = messageInfo.message;
-        countdownText.color = messageInfo.color * 0.25f + Color.black * 0.75f;
+        rimText.color = messageInfo.color * 0.25f + Color.black * 0.75f;
+        countdownText.color = messageInfo.color;
         
         canvasGroup.alpha = 1f;
         textTransform.localScale = Vector3.one;
         
+        yield return new WaitForSeconds(fullDuration * 0.25f);
+        
         float startTime = Time.time;
-        float duration = fullDuration / 2f;
-        while (Time.time - startTime > duration)
+        float duration = fullDuration * 0.75f;
+        while (Time.time - startTime < duration)
         {
             canvasGroup.alpha = Sinerp(1f, 0f, (Time.time - startTime) / duration);
             textTransform.localScale = Vector3.one * Sinerp(1f, 0f, (Time.time - startTime) / duration);
+            Debug.Log(canvasGroup.alpha);
             yield return null;
         }
         canvasGroup.alpha = 0f;
